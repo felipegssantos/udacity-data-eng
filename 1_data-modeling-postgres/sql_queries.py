af513b821d377219c4981b8b412650b0913a820a
Felipe Santos <felipegssantos@gmail.com>
2020-01-30 00:23:12 -0300

    Write SQL queries for ETL

--- 1_data-modeling-postgres/sql_queries.py
+++ 1_data-modeling-postgres/sql_queries.py
@@ -1,49 +1,84 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id text,
+                                                                  start_time time,
+                                                                  user_id text,
+                                                                  level text,
+                                                                  song_id text,
+                                                                  artist_id text,
+                                                                  session_id text,
+                                                                  location text,
+                                                                  user_agent text);
+""")
+# TODO: evaluate usage of other data types instead of "time" for column "start_time"
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id text,
+                                                          first_name text,
+                                                          last_name text,
+                                                          gender char(1),
+                                                          level text);
 """)
 
-user_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id text,
+                                                          title text,
+                                                          artist_id text,
+                                                          year int,
+                                                          duration numeric);
 """)
 
-song_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id text,
+                                                              name text,
+                                                              location text,
+                                                              latitude numeric,
+                                                              longitude numeric);
 """)
 
-artist_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time time,
+                                                         hour int,
+                                                         day int,
+                                                         week int,
+                                                         month int,
+                                                         year int,
+                                                         weekday varchar(9));
 """)
-
-time_table_create = ("""
-""")
+# TODO: consider enumerated data type for weekday and month
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, 
+                                                   artist_id, session_id, location, user_agent)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+                        VALUES (%s, %s, %s, %s, %s);
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
+                        VALUES (%s, %s, %s, %s, %s);
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
+                          VALUES (%s, %s, %s, %s, %s);
 """)
 
-
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT songs.song_id, songs.artist_id
+                  FROM songs JOIN artists ON songs.artist_id = artists.artist_id
+                  WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s;
 """)
 
 # QUERY LISTS
