cdf69a09cf3a1934b7744859ef35f3f92eda5b26
Felipe Santos <felipegssantos@gmail.com>
2020-02-01 23:24:06 -0300

    Parse CSV files and insert into cassandra tables

---
+++ 2_data-modeling-cassandra/etl.py
@@ -0,0 +1,61 @@
+import csv
+from glob import glob
+import os
+
+from cassandra.cluster import Cluster
+
+from queries import insert_session_history, insert_song_history, insert_user_history
+
+
+def connect_to_cassandra(keyspace='sparkifydb'):
+    """
+    Connects to Sparkify's Cassandra database.
+
+    :param keyspace: name of the keyspace
+    :return: cassandra.Cluster and cassandra.Session instances for this connection
+    """
+    cluster = Cluster()
+    session = cluster.connect()
+    session.set_keyspace(keyspace)
+    return cluster, session
+
+
+def process_event_data(session, csv_path):
+    """
+    Reads all data from `filepath` and loads it to Apache Cassandra.
+
+    :param session: cassandra.Session instance
+    :param csv_path: path to a directory of CSV files containing event data
+    """
+    event_stream = stream_from_files(csv_path)
+    for event in event_stream:
+        session.execute(insert_session_history, (event['artist'], event['song'], event['length'],
+                                                 event['sessionId'], event['itemInSession']))
+        session.execute(insert_user_history, (event['artist'], event['song'], event['firstName'], event['lastName'],
+                                              event['userId'], event['sessionId'], event['itemInSession']))
+        session.execute(insert_song_history, (event['song'], event['firstName'], event['lastName']))
+
+
+def stream_from_files(csv_path):
+    """
+    Iterates over CSV files in `csv_path` line by line until all files have been consumed.
+
+    :param csv_path: directory containing CSV event data
+    :return: iterator of dict with event data
+    """
+    csv_files = glob(os.path.join(csv_path, '*.csv'))
+    num_files = len(csv_files)
+    for i, f in enumerate(csv_files, start=1):
+        print(f'Processing file {i}/{num_files}')
+        with open(f, 'r', encoding='utf8', newline='') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            header = next(csv_reader)
+            for line in csv_reader:
+                yield {k: v for k, v in zip(header, line)}
+
+
+if __name__ == '__main__':
+    cluster, session = connect_to_cassandra()
+    process_event_data(session, f'{os.getcwd()}/event_data')
+    session.shutdown()
+    cluster.shutdown()
