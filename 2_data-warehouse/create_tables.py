782f9754b5741c57f1e88bdc45949573ca399fd3
Felipe Santos <felipegssantos@gmail.com>
2020-02-14 20:44:12 -0300

    Add missing docstrings

--- 2_data-warehouse/create_tables.py
+++ 2_data-warehouse/create_tables.py
@@ -4,18 +4,36 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Deletes pre-existing tables to ensure our tables will be created without the database throwing errors.
+
+    :param cur: a psycopg2 cursor for the database
+    :param conn: a psycopg2 connection to the database
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates staging tables, fact table and dimension tables in the data warehouse.
+
+    :param cur: a psycopg2 cursor for the database
+    :param conn: a psycopg2 connection to the database
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Runs the whole table creation routine.
+
+    This function will first connect to the database, then drop all pre-existing tables and finally
+    create all tables necessary for the data warehouse.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- 2_data-warehouse/etl.py
+++ 2_data-warehouse/etl.py
@@ -4,6 +4,12 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Extracts raw data from S3 buckets and loads it to staging tables
+
+    :param cur: a psycopg2 cursor for the database
+    :param conn: a psycopg2 connection to the database
+    """
     for query in copy_table_queries:
         print('  Running:', end='')
         print(query)
@@ -12,6 +18,13 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+    Extracts data from staging tables, transforms it to match the star schema and loads the result to
+    fact and dimension tables.
+
+    :param cur: a psycopg2 cursor for the database
+    :param conn: a psycopg2 connection to the database
+    """
     for query in insert_table_queries:
         print('  Running:', end='')
         print(query)
@@ -20,6 +33,12 @@ def insert_tables(cur, conn):
 
 
 def main():
+    """
+    Runs whole ETL pipeline.
+
+    This function will first connect to the database, then copy raw data to staging tables and finally
+    extract and transform data from staging tables to the fact and dimension tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
